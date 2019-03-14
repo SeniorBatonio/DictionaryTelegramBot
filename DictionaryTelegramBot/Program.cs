479d842b14be7f1f50c867bf4c0954674940840c
SeniorBatonio <opupep06@gmail.com>
2019-03-14 14:33:45 +0200

    Добавьте файлы проекта.

---
+++ DictionaryTelegramBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace DictionaryTelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("784574297:AAHU6DEHTHaTf8UrkSaAir9oPXPm5oVU0pc");
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Hello! I'm user {me.Id} and my name is {me.FirstName}.");
+            Console.ReadLine();
+        }
+    }
+}
